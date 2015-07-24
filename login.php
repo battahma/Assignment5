6aadbdee715219d78a54d3c469f91e00a762b01f
Chris Bakkom <chris.bakkom@gmail.com>
2015-07-23 18:39:31 -0700

    Added Login as Guest

--- login.php
+++ login.php
@@ -15,19 +15,47 @@ echo "<br>\n";
 echo "<br>\n";
 echo "<br>\n";
 /*http://web.engr.oregonstate.edu/~osterbit/2/repo/class-content/form_tests/Formtest.php*/
-echo "<form style=\"text-align: center\" action=\"content1.php\" method=\"POST\">\n";
+echo "<form id=\"LoginForm\" style=\"text-align: center\" action=\"content1.php\" method=\"POST\">\n";
 echo "<table style=\"margin: auto\">";
 echo "<tr>";
 echo "<td>Login Name:</td>";
-echo "<td><input type=\"text\" name=\"username\"></td>\n";
+echo "<td><input id=\"username\" type=\"text\" name=\"username\"></td>\n";
 echo "</tr>";
 echo "<tr>";
 echo "<td>Password: </td>";
-echo "<td><input type=\"password\" name=\"password\"></td>";
+echo "<td><input id=\"password\" type=\"password\" name=\"password\"></td>";
 echo "</tr>";
 echo "</table>";
 echo "<input type=\"submit\" value=\"Submit\">\n";
 echo "</form>\n";
+
+	//The Guest Button
+echo "
+	<div class=\"wrapper\">
+	<button type=\"button\" 
+	id=\"GuestButton\" 
+	onclick = \"GuestLogin()\">
+	Login as Guest
+	</button>
+	</div>";
+
+	//A little bit of styling to center that Button
+echo "
+	<style type=\"text/css\">
+	div.wrapper{
+		text-align: center;
+	}
+	</style>";
+
+	//function to Login as a Guest
+echo "<script type=\"text/javascript\">
+	function GuestLogin(){
+	document.getElementById(\"username\").value = \"Guest\";
+	document.getElementById(\"password\").value = \"\";
+	document.getElementById(\"LoginForm\").submit();
+	}
+	</script>
+";
 
 echo "</body>\n";
 echo "</html>\n";
