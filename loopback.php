519248528dfd26d3a22dd38128b95a357c38bd57
Tyler Koistinen <koistinentyler@gmail.com>
2015-07-28 18:17:31 -0700

    Fixed Loopback

--- loopback.php
+++ loopback.php
@@ -1,19 +1,33 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-type: text/plain');
 
-header('Content of type text or NULL');
-
-if($_GET != NULL){
-    $jsonObject = json_encode($_GET);
-    echo $jsonObject;
-    return $jsonObject;
-    }
-    
-    
-if($_POST != NULL){
-    $jsonObject = json_encode($_POST);
-    echo $jsonObject;
-    return $jsonObject;
-    }
-    
-    
+if(isset($_POST)){
+	$displayObj["Type"] = "POST";
+	if($_POST != NULL){
+		$displayObj["parameters"] = $_POST;
+	}
+	else{
+		$displayObj["parameters"] = NULL;
+	}
+	$jsonObject = json_encode($displayObj);
+	echo $jsonObject;
+	
+}
+elseif (isset($_GET)){
+	return $jsonObject;
+	$displayObj["Type"] = "GET";
+	if($_GET != NULL){
+		$displayObj["parameters"] = $_GET;
+	}
+	else{
+		$displayObj["parameters"] = NULL;
+	}
+	$jsonObject = json_encode($displayObj);
+	echo $jsonObject;
+	return $jsonObject;
+}
+	
+	
 ?>
